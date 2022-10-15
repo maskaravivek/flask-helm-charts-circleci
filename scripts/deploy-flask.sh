a0cb6423002e4c74e44321e4f902d466ce03ebd9
Vivek Maskara <maskaravivek@gmail.com>
2022-10-14 21:08:54 -0400

    Minor

--- scripts/deploy-flask.sh
+++ scripts/deploy-flask.sh
@@ -2,7 +2,7 @@
 TAG=$1
 AWS_ECR_ACCOUNT_URL=$2
 AWS_ECR_HELM_REPO_NAME=$3
-echo "start tiller"
+echo "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME"
 export KUBECONFIG=$HOME/.kube/config
 result=$(eval helm ls | grep flask-helm) 
 if [ $? -ne "0" ]; then 
